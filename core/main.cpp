dfc6bdbb47d4c38e08831d70c59d2e4daeef14fb
Ladislav Foldyna <foldyna@gmail.com>
2021-08-05 14:06:26 +0200

    Database backup and Splashscreen
    Database is exported to ADIF during starting phase (before migration)
    Splashscreen was added because it can take some time to export, migrate
    the database.

--- core/main.cpp
+++ core/main.cpp
@@ -11,12 +11,14 @@
 #include <QSharedMemory>
 #include <QMessageBox>
 #include <QDebug>
+#include <QSplashScreen>
 
 #include "Migration.h"
 #include "ui/MainWindow.h"
 #include "Rig.h"
 #include "Rotator.h"
 #include "AppGuard.h"
+#include "logformat/AdiFormat.h"
 
 static void loadStylesheet(QApplication* app) {
     QFile style(":/res/stylesheet.css");
@@ -59,6 +61,61 @@ static bool openDatabase() {
     }
 }
 
+static bool backupDatabase()
+{
+    /* remove old backups */
+    /* retention time is 30 days but a minimum number of backup files is 5 */
+    const int retention_time = 30;
+    const int min_backout_count = 5;
+
+    QDir dir(QStandardPaths::writableLocation(QStandardPaths::DataLocation));
+    QString path = dir.filePath("qlog_backup_" + QDateTime::currentDateTime().toString("yyyyMMddhhmmss") + ".adi");
+    QString filter("qlog_backup_%1%1%1%1%1%1%1%1%1%1%1%1%1%1.adi");
+    filter = filter.arg("[0123456789]");
+
+    QFileInfoList file_list = QDir(dir).entryInfoList(QStringList(filter), QDir::Files, QDir::Name);
+
+    /* Keep the minimum number of backups */
+    /* If a number of backup is greater than min_backout_count then remove files older 30 days but always
+       protect a minimum number of backup file files */
+    for (int i = 0; i < qMin(min_backout_count, file_list.size()); i++)
+    {
+        file_list.takeLast();  // remove last 5 files from sorted list by name to protect them from removing.
+    }
+
+    /* remove those older than 30 days from the remaining files. */
+    Q_FOREACH (auto fileInfo, file_list)
+    {
+        if (fileInfo.birthTime().date().daysTo(QDate::currentDate()) > retention_time)
+        {
+            QString filepath = fileInfo.absoluteFilePath();
+            QDir deletefile;
+            deletefile.setPath(filepath);
+            deletefile.remove(filepath);
+        }
+    }
+
+    /* make a backup file */
+    QFile backup_file(path);
+
+    if ( !backup_file.open(QFile::ReadWrite | QIODevice::Text))
+    {
+        qWarning()<<"Cannot open backup file " << path << "for writing";
+        return false;
+    }
+
+    qDebug()<<"Exporting a Database backup to " << path;
+
+    QTextStream stream(&backup_file);
+    AdiFormat adi(stream);
+
+    adi.runExport();
+    stream.flush();
+    backup_file.close();
+
+    qDebug()<<"Database backup finished";
+    return true;
+}
 static bool migrateDatabase() {
     Migration m;
     return m.run();
@@ -142,12 +199,23 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    QPixmap pixmap(":/res/qlog.png");
+    QSplashScreen splash(pixmap);
+    splash.show();
+
     createDataDirectory();
 
     if (!openDatabase()) {
         QMessageBox::critical(nullptr, QMessageBox::tr("QLog Error"),
                               QMessageBox::tr("Could not connect to database."));
         return 1;
+    }
+
+    /* a migration can break a database therefore a backup is call before it */
+    if (!backupDatabase())
+    {
+        QMessageBox::critical(nullptr, QMessageBox::tr("QLog Error"),
+                              QMessageBox::tr("Could not export a QLog database to ADIF as a backup.<p>Try to export your log to ADIF manually"));
     }
 
     if (!migrateDatabase()) {
@@ -161,6 +229,7 @@ int main(int argc, char* argv[]) {
 
     MainWindow w;
     QIcon icon(":/res/qlog.png");
+    splash.finish(&w);
     w.setWindowIcon(icon);
     w.show();
 
