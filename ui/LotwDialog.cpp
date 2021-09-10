11db67043ea87727b48587f9dd68de7bd1f12639
Ladislav Foldyna <foldyna@gmail.com>
2021-09-10 16:08:01 +0200

    LoTW: Added missing UPDATE of LoTW QSL SENT status and date

--- ui/LotwDialog.cpp
+++ ui/LotwDialog.cpp
@@ -104,21 +104,22 @@ void LotwDialog::upload() {
                            "       sat_name, station_callsign, operator, "
                            "       rst_sent, rst_rcvd, my_state, my_cnty, "
                            "       my_vucc_grids "
-                           "FROM contacts "
-                           "WHERE (lotw_qsl_sent <> 'Y' OR lotw_qsl_sent is NULL) "
+                           "FROM contacts ";
+    QString query_where =  "WHERE (lotw_qsl_sent <> 'Y' OR lotw_qsl_sent is NULL) "
                            "      AND (prop_mode NOT IN ('INTERNET', 'RPT', 'ECH', 'IRL') OR prop_mode IS NULL) ";
+    QString query_order = " ORDER BY start_time ";
 
     if ( !ui->myCallsignCombo->currentText().isEmpty() )
     {
-        query_string.append(" AND station_callsign = '" + ui->myCallsignCombo->currentText() + "'");
+        query_where.append(" AND station_callsign = '" + ui->myCallsignCombo->currentText() + "'");
     }
 
     if ( !ui->myGridCombo->currentText().isEmpty() )
     {
-        query_string.append(" AND my_gridsquare = '" + ui->myGridCombo->currentText() + "'");
+        query_where.append(" AND my_gridsquare = '" + ui->myGridCombo->currentText() + "'");
     }
 
-    query_string.append(" ORDER BY start_time ");
+    query_string = query_string + query_where + query_order;
 
     qCDebug(runtime) << query_string;
 
@@ -152,13 +153,20 @@ void LotwDialog::upload() {
             if ( lotw.uploadAdif(data, ErrorString) == 0 )
             {
                 QMessageBox::information(this, tr("QLog Information"), tr("%n QSO(s) uploaded.", "", count));
-                //update contact set lotw_sent = y, lotwsdate curr_date
+
+                query_string = "UPDATE contacts "
+                               "SET lotw_qsl_sent='Y', lotw_qslsdate = strftime('%Y-%m-%d',DATETIME('now', 'utc')) "
+                               + query_where;
+
+                qCDebug(runtime) << query_string;
+
+                QSqlQuery query_update(query_string);
+                query_update.exec();
             }
             else
             {
                 QMessageBox::critical(this, tr("LoTW Error"), ErrorString);
             }
-
         }
     }
     else {
