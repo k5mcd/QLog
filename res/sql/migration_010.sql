0243dc7981cdb362468042db4b69bbee93dabb97
Ladislav Foldyna <foldyna@gmail.com>
2022-06-21 15:49:03 +0200

    ADIF 3.1.3: Changed Mode and Submode enumerations
    - Addition of new Submodes C4FM, DMR, & DSTAR to Mode
      DIGITALVOICE and deprecation of C4FM and DSTAR as Modes.
    - Addition of new Mode DYNAMIC and Submodes VARA HF,
      VARA SATELLITE, VARA FM 1200, & VARA FM 9600.

--- res/sql/migration_010.sql
+++ res/sql/migration_010.sql
@@ -1,1 +1,8 @@
 ALTER TABLE bands ADD last_seen_freq FLOAT;
+
+DELETE FROM modes WHERE name IN ('C4FM', 'DMR', 'DSTAR');
+UPDATE modes SET submodes = '["C4FM", "DMR", "DSTAR"]' WHERE name = 'DIGITALVOICE';
+
+INSERT INTO modes (name, rprt, dxcc, enabled, submodes) VALUES
+('DYNAMIC', NULL, 'DIGITAL', false, '["VARA HF", "VARA SATELLITE", "VARA FM 1200", "VARA FM 9600"]')
+ON CONFLICT DO NOTHING;
