a186db54947ae81fe206a2b9d29bf7aaa0586132
Ladislav Foldyna <foldyna@gmail.com>
2022-10-03 15:51:22 +0200

    QT6 Migration: Added QtKeyChain version conditionals

--- core/CredentialStore.cpp
+++ core/CredentialStore.cpp
@@ -1,5 +1,9 @@
 #include <QEventLoop>
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
 #include <qt5keychain/keychain.h>
+#else
+#include <qt6keychain/keychain.h>
+#endif
 #include <QApplication>
 #include "CredentialStore.h"
 #include "core/debug.h"
