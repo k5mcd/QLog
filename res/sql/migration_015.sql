083f4d02808120b50408bb721c8d2f21c64a1b3e
Ladislav Foldyna <foldyna@gmail.com>
2022-12-25 13:48:54 +0100

    ADIF 3.1.4: Added a support for submm Band - Bandmap is not shown for submm

--- res/sql/migration_015.sql
+++ res/sql/migration_015.sql
@@ -1,1 +1,4 @@
 UPDATE modes SET submodes = '["C4FM", "DMR", "DSTAR", "FREEDV", "M17" ]' WHERE name = 'DIGITALVOICE';
+
+INSERT INTO bands (name, start_freq, end_freq, enabled) VALUES
+('submm',300000.0, 7500000.0, 0);
