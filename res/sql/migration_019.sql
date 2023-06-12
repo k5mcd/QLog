4f6a677612608f4ab8760f5d8cad286d2e339335
Ladislav Foldyna <foldyna@gmail.com>
2023-06-12 16:27:30 +0200

    Fixed #213 - CONTESTIA mode should be CONTESTI

--- res/sql/migration_019.sql
+++ res/sql/migration_019.sql
@@ -2,3 +2,5 @@ UPDATE contacts SET mode = 'MFSK', submode = 'FT4' WHERE mode = 'FT4';
 UPDATE contacts SET mode = 'MFSK', submode = 'FST4' WHERE mode = 'FST4';
 UPDATE contacts SET mode = 'MFSK', submode = 'FST4W' WHERE mode = 'FST4W';
 UPDATE contacts SET mode = 'MFSK', submode = 'Q65' WHERE mode = 'Q65';
+
+UPDATE modes SET name = 'CONTESTI' WHERE name = 'CONTESTIA';
