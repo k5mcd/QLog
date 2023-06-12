b8eae2bdf6026017b6cd8c37c99301b6efb8715c
Ladislav Foldyna <foldyna@gmail.com>
2023-06-12 16:24:08 +0200

    Fixed #212 - Incorrect FT4 mode/submode

--- core/Migration.h
+++ core/Migration.h
@@ -38,7 +38,7 @@ private:
     bool createTriggers();
     QString fixIntlField(QSqlQuery &query, const QString &columName, const QString &columnNameIntl);
 
-    static const int latestVersion = 18;
+    static const int latestVersion = 19;
 };
 
 #endif // MIGRATION_H
---
+++ res/sql/migration_019.sql
@@ -0,0 +1,4 @@
+UPDATE contacts SET mode = 'MFSK', submode = 'FT4' WHERE mode = 'FT4';
+UPDATE contacts SET mode = 'MFSK', submode = 'FST4' WHERE mode = 'FST4';
+UPDATE contacts SET mode = 'MFSK', submode = 'FST4W' WHERE mode = 'FST4W';
+UPDATE contacts SET mode = 'MFSK', submode = 'Q65' WHERE mode = 'Q65';
