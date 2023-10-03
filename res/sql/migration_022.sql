d17d3d6b4929c9089726e59839a42d49d7d9a34b
Ladislav Foldyna <foldyna@gmail.com>
2023-10-03 10:37:42 +0200

    DB: Cleanup and speedup

--- res/sql/migration_022.sql
+++ res/sql/migration_022.sql
@@ -6,3 +6,8 @@ ALTER TABLE main_layout_profiles ADD detail_col_C TEXT;
 ALTER TABLE main_layout_profiles ADD main_geometry TEXT;
 ALTER TABLE main_layout_profiles ADD main_state TEXT;
 
+DROP TABLE IF EXISTS contacts_old;
+
+CREATE INDEX IF NOT EXISTS start_time_idx ON contacts(start_time);
+CREATE INDEX IF NOT EXISTS band_idx ON contacts(band);
+CREATE INDEX IF NOT EXISTS mode_idx ON contacts(mode);
